0b9e8e788b5cd361fa7e92979733d159d222f79c
michetonu <michetonu@gmail.com>
2017-06-19 16:41:18 +0200

    updated code

--- lda_papers.py
+++ lda_papers.py
@@ -10,56 +10,56 @@ import textmining
 import string
 import os
 import sys  
+import pickle
 
 root = '/home/mtonutti/Documents/latent_dirchilet_allocation/'
 
 tokenizer = RegexpTokenizer(r'\w+')
 en_stop = get_stop_words('en')
 p_stemmer = PorterStemmer()
+#dictionary = open("words_alpha.txt").read() #Uncomment if using the dictionary
 
 print("Loading text(s)...")
 print()
 
+#Read texts
 documents = []
+titles = []
 
 file_root = root+'NLM_500/documents'
 for filename in os.listdir(file_root):
     if filename.endswith(".txt"): 
     	os.chdir(file_root)
-    	text = open(filename,errors='ignore').read().lower()
+    	text = open(filename,errors='ignore').read()
     	documents.append(text)
+    	titles.append(text.split('\n', 1)[0]) #Get title of paper
+    	text.lower()
 
+
+os.chdir(root)
 
 clean_documents = []
 tdm = textmining.TermDocumentMatrix()
 
-os.chdir(root)
-dictionary = open("words_alpha.txt").read()
 
 doc_count = 0
 for doc in documents:
-	#Tokenize each document
-	tokens = tokenizer.tokenize(doc)
-	
-	#Remove useless words
-	tokens = [i for i in tokens if not i in en_stop]
+	tokens = tokenizer.tokenize(doc) 	# Tokenize each document
 
-	#Only keep words in english dictionary (SLOW)
-	#tokens = [a for a in tokens if a in dictionary]
+	tokens = [i for i in tokens if not i in en_stop] 	# Remove useless words
 
-	#Only keep words longer than 3 letters
-	tokens = [a for a in tokens if len(a) > 3]
+	#tokens = [a for a in tokens if a in dictionary] 	# Only keep words in the english dictionary (SLOW)
 
-	#Only printable words 
-	#printable = set(string.printable)
+	tokens = [a for a in tokens if len(a) > 3] 	#Only keep words longer than 3 letters
+
+	#printable = set(string.printable) 	# Keep only printable words 
 	#filter(lambda x: x in printable, tokens)
 
-	#Stem words (remove endings to find shared meanings)
-	tokens = [p_stemmer.stem(i) for i in tokens]
+	tokens = [p_stemmer.stem(i) for i in tokens] 	# Stem words (remove endings to find shared meanings)
 	clean_documents.append(tokens)
 
-	tkstring = ' '.join(tokens)
-	tdm.add_doc(tkstring)
+	tkstring = ' '.join(tokens) 	# Reformat for term-document matrix
+	tdm.add_doc(tkstring)	# Add processed text to term-document matrix
 	doc_count += 1
 	print("Finished processing document {} out of {}".format(doc_count,len(documents)))
 
@@ -68,24 +68,46 @@ print("Creating term-document matrix...")
 print()
 
 
-X = np.zeros([doc_count,len(vocab)])
+# Create numpy array from TDM
 
 vocab = []
+r = 0
 for row in tdm.rows():
 	if vocab == []:
 		vocab = row
+		X = np.zeros([doc_count,len(vocab)])
 	else:
 		X[r-1,:] = row
+	r+=1
 
 X = X.astype(int)
 
-print("Fitting the LDA...")
+train_test_ratio = 0.90
+ntrain = len(X*train_test_ratio)
+X_train = X[:ntrain,:]
+X_test = X[ntrain:,:]
+
+# Train LDA
+print("Training the LDA...")
 print()
 model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-model.fit(X)
+model.fit(X_train)
 
+# Display results
 topic_word = model.topic_word_
 n_top_words = 8
 for i, topic_dist in enumerate(topic_word):
 	topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
-	print('Topic {}: {}'.format(i+1, ' '.join(topic_words)))
+	print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+
+print(model.components_)
+#Save model
+with open("dla_papers.sav", 'wb') as ff:
+	pickle.dump(model, ff)
+
+doc_topic = model.doc_topic_
+
+for i in range(10):
+	print("{} (top topic: {})".format(titles[i], doc_topic[i].argmax()))
+
+
